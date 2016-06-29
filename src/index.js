4af3a82a33f033b3a996c0a199a2175fbb1cf51b
Kent <kentqlee@gmail.com>
2016-06-28 18:22:13 -0700

    Add GetTimeEstimate function

--- src/index.js
+++ src/index.js
@@ -29,6 +29,29 @@ const Uber = {
         callback(null, body);
       }
     });
+  },
+
+  GetTimeEstimate: (queryParams, callback) => {
+    request({
+      url: 'https://api.uber.com/v1/estimates/time',
+      headers: {
+        Authorization: 'Token ' + uberServerToken,
+      },
+      qs: {
+        start_latitude: queryParams.start_latitude,
+        start_longitude: queryParams.start_longitude,
+        end_latitude: queryParams.end_latitude,
+        end_longitude: queryParams.end_longitude 
+      },
+      method: 'GET',
+    }, (err, res, body) => {
+      if (err) {
+        callback(err);
+      }
+      if (callback) {
+        callback(null, body);
+      }
+    });
   }
 
 };
